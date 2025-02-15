89b8420091a1d38c8e8eabd5e5b4344f81f3f2f8
Nahid Hasan Noyon <nahidhasannoyon20@gmail.com>
2025-02-15 11:09:21 +0600

    feat: add new image assets and create Asset constants for easier access

---
+++ lib/app/core/constants/assets.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Asset {
+  Asset._();
+  
+  /// Assets for imagesAndroid
+  /// assets/images/android.png
+  static const String imagesAndroid = "assets/images/android.png";
+
+  /// Assets for imagesBanner
+  /// assets/images/banner.png
+  static const String imagesBanner = "assets/images/banner.png";
+
+  /// Assets for imagesCall
+  /// assets/images/call.png
+  static const String imagesCall = "assets/images/call.png";
+
+  /// Assets for imagesDots
+  /// assets/images/dots.jpg
+  static const String imagesDots = "assets/images/dots.jpg";
+
+  /// Assets for imagesPhone
+  /// assets/images/phone.png
+  static const String imagesPhone = "assets/images/phone.png";
+
+  /// Assets for imagesPoster
+  /// assets/images/poster.png
+  static const String imagesPoster = "assets/images/poster.png";
+}
+
